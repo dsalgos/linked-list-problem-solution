738ad470b2e3ba6aef678e5877b263e0ea1ac3fa
ds algos <ds.algos143@gmail.com>
2021-08-13 09:21:18 +0530

    Update NthNodeFromTail.java
    Commit solution for nth node from last of the singly linked list.

--- src/org/dsalgos/linked/list/problem/solutions/NthNodeFromTail.java
+++ src/org/dsalgos/linked/list/problem/solutions/NthNodeFromTail.java
@@ -1,5 +1,28 @@
 package org.dsalgos.linked.list.problem.solutions;
 
-public class NthNodeFromTail {
+import org.ds.algos.datasructures.node.Node;
+import org.ds.algos.datastructures.singlylinked.list.LinkedList;
 
+public class NthNodeFromTail {
+	
+	public E findNthNodeFromTail(int indexFromTail) {
+		Node<E> node = head;
+		Node<E> fast = head;
+		
+		if(indexFromTail < 0 || indexFromTail >= size()) {
+			throw new ArrayIndexOutOfBoundsException(indexFromTail);
+		}
+		
+		int index = 0;
+		for(; index < indexFromTail; index++) {
+			fast = fast.getNext();
+		}
+		
+		for(; fast != null; fast = fast.getNext()) {
+			node = node.getNext();
+		}
+		
+		return node.getData();
+	}
+	
 }
